54930fc9ae57935825c9741e51b9a3cd94277c23
Justin Senia <justinsenia342@gmail.com>
2019-09-22 14:13:41 -0400

    REMOVE INLINE JS

---
+++ js/ProtoFunctions.js
@@ -0,0 +1,6 @@
+var name;
+
+function askUserNameAndDisplay(){
+	var n = prompt("Why Allo' Thar! Whuts Yer' Name Son? ", "Tell the strange man your name");
+	alert("Hello There " + n + "! Howsit Goin'!?!");
+	}
