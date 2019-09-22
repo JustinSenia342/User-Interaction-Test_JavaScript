7698ba76176c4ca55668cfa4f55a411e22723f54
Justin Senia <justinsenia342@gmail.com>
2019-09-22 15:24:45 -0400

    FIX FORMATTING

--- js/ProtoFunctions.js
+++ js/ProtoFunctions.js
@@ -3,4 +3,4 @@ var name;
 function askUserNameAndDisplay(){
 	var n = prompt("Why Allo' Thar! Whuts Yer' Name Son? ", "Tell the strange man your name");
 	alert("Hello There " + n + "! Howsit Goin'!?!");
-	}
+}
